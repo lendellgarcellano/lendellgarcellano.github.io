8ce64eb712b3f5fdcb24a5100c2cc06b3cfa80b3
lendellgarcellano <joselendell@gmail.com>
2025-12-02 13:09:04 +0800

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -12,3 +12,17 @@ document.querySelectorAll(".nav-links a").forEach(link => {
     link.classList.add("active");
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const current = window.location.pathname.split("/").pop(); // filename.html
+
+  document.querySelectorAll(".nav-links a").forEach(link => {
+    const linkPage = link.getAttribute("href");
+
+    if (current === "" && linkPage === "index.html") {
+      link.classList.add("active");
+    } else if (linkPage === current) {
+      link.classList.add("active");
+    }
+  });
+});
