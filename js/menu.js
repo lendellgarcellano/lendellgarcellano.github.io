cb5e39b25aec6827e0834770cc14ff7e5effb60b
lendellgarcellano <joselendell@gmail.com>
2025-12-02 13:16:14 +0800

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,4 @@
 
-const menuToggle = document.getElementById("menuToggle");
-const navLinks = document.getElementById("navLinks");
-
-menuToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
-
-
-document.querySelectorAll(".nav-links a").forEach(link => {
-  if (link.href === window.location.href) {
-    link.classList.add("active");
-  }
-});
-
 document.addEventListener("DOMContentLoaded", () => {
   // Load menu.html dynamically
   fetch("menu.html")
